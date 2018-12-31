9851ca20d0e8d061513eb2477359240bad508684
PrakashAbd <prakazsh1@gmail.com>
2018-12-31 14:36:45 +0530

    frst

---
+++ src/main/java/com/example/Orphanage/OrphanageApplication.java
@@ -0,0 +1,12 @@
+package com.example.Orphanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrphanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrphanageApplication.class, args);
+	}
+}
